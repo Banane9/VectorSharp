80fbcbe987eea0021279cc9d686683a4269cd975
Arne Kiesewetter <banane1996@gmx.net>
2015-11-05 22:38:12 +0100

    Test that the vectors for all number types create successfully

--- VectorSharp.Tests/GenericVector3.cs
+++ VectorSharp.Tests/GenericVector3.cs
@@ -1,16 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Reflection;
 
 namespace VectorSharp.Tests
 {
     [TestClass]
     public class GenericVector3
     {
+        private readonly MethodInfo createMethod = typeof(Generic.Vector3).GetTypeInfo().GetDeclaredMethod("Create");
+
+        private readonly Type[] numberTypes = new[]
+        {
+            typeof(byte), typeof(sbyte),
+            typeof(short), typeof(ushort),
+            typeof(int), typeof(uint),
+            typeof(long), typeof(ulong),
+            typeof(float), typeof(double)
+        };
+
         [TestMethod]
         public void Creates()
         {
-            var vec3 = Generic.Vector3.Create<double>(1, 1, 1);
-            Assert.IsTrue(true);
+            foreach (var numberType in numberTypes)
+            {
+                createMethod.MakeGenericMethod(numberType)
+                    .Invoke(null, new object[]
+                    {
+                        numberType.GetField("MinValue").GetValue(null),
+                        numberType.GetField("MaxValue").GetValue(null),
+                        numberType.GetField("MaxValue").GetValue(null)
+                    });
+            }
         }
     }
 }
